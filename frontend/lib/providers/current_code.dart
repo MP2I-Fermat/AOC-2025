10903a202c732be01aece27dca6658a664852070
Mylo Fawcett <mylo.fawcett@gmail.com>
2025-11-16 20:55:41 +0100

    Update starter program

--- frontend/lib/providers/current_code.dart
+++ frontend/lib/providers/current_code.dart
@@ -13,29 +13,21 @@ class CodeProvider extends Notifier<String> {
     }
 
     return '''
-..
-Huitr - a purely functional programming language.
-Copyright (C) 2025  jd-develop
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-..
-
 . A simple fizzbuzz
 
 [
-  0 > getarg > n  . stores the current value into n
+  args, 0 > std::list::nth > n  . stores the current value into n
 
   . trivial and self-explanatory /s
-  n, 15 > std::math::mod, 0 > std::comp::eq,
-    ["fizzbuzz" > std::out::print_string],
-    [n, 5 > std::math::mod, 0 > std::comp::eq,
-      ["buzz" > std::out::print_string],
-      [n, 3 > std::math::mod, 0 > std::comp::eq,
-        ["fizz" > std::out::print_string],
-        [n > std::out::print_int] > if] > if] > if
+  > (n, 15 > std::math::mod, 0 > std::comp::eq,
+    ["fizzbuzz" > std::io::print],
+    [> (n, 5 > std::math::mod, 0 > std::comp::eq,
+      ["buzz" > std::io::print],
+      [> (n, 3 > std::math::mod, 0 > std::comp::eq,
+        ["fizz" > std::io::print],
+        [n > std::io::print] > std::tests::if)] > std::tests::if)] > std::tests::if)
 
-  n, 100 > std::comp::eq, [()], [n, 1 > std::math::add > rec] > if
+  > (n, 100 > std::comp::eq, [()], [n, 1 > std::math::add > fizzbuzz] > std::tests::if)
 ] > fizzbuzz
 
 0 > fizzbuzz
