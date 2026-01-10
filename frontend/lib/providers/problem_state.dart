bb5a549a674734500594106607ac22b01dee17a9
Mylo Fawcett <mylo.fawcett@gmail.com>
2026-01-10 15:55:14 +0100

    Autosave problem solutions

--- frontend/lib/providers/problem_state.dart
+++ frontend/lib/providers/problem_state.dart
@@ -1,5 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:common/protocol.dart';
 import 'package:frontend/providers/connection.dart';
+import 'package:frontend/providers/saved_code.dart';
 import 'package:jaspr_riverpod/jaspr_riverpod.dart';
 
 final problemStateProvider = NotifierProvider(ProblemStateNotifier.new);
@@ -9,6 +11,32 @@ class ProblemStateNotifier extends Notifier<Map<String, ProblemInfo>> {
   Map<String, ProblemInfo> build() {
     ref.listen(connectionMessagesProvider, (_, message) {
       if (message.value case ProblemsUpdate(:final problems)) {
+        final previousProblems = state.entries
+            .where((p) => p.value.unlocked)
+            .map((p) => p.key)
+            .sorted();
+
+        if (previousProblems.isNotEmpty) {
+          final orderedProblems = problems.entries
+              .where((p) => p.value.unlocked)
+              .map((p) => p.key)
+              .sorted();
+
+          final previousLastIndex = orderedProblems.indexOf(
+            previousProblems.last,
+          );
+
+          if (previousLastIndex != -1 &&
+              previousLastIndex < orderedProblems.length - 1) {
+            final name =
+                'Sauvegarde automatique: Solution partie ${previousProblems.length}';
+
+            if (!ref.read(slotProvider).any((slot) => slot.name == name)) {
+              ref.read(slotProvider.notifier).createNewSlot(name);
+            }
+          }
+        }
+
         state = problems;
       }
     });
